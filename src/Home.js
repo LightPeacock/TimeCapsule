19103d327ff19931d37c8a185d5689745de5683e
C L Narayanan <clnarayanan@C-ka-MacBook-Air.local>
2024-05-22 18:56:47 +0530

    First Commit

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Home</h1><br/><br/><br/>
+        <div className="text-content">
+          <p>Welcome to our nostalgic corner! Journey back in time with us as we unveil cherished memories captured through the lens of childhood.
+            Our website is a digital scrapbook, a treasure trove of moments frozen in time, each photograph a window to the past.
+            From the innocence of infancy to the mischief of youth, every image tells a story, igniting a whirlwind of emotions and nostalgia.
+            Dive into the depths of your past, reliving carefree days and heartfelt laughter. Whether it's the first steps, birthday celebrations, or family vacations, our collection encapsulates the essence of growing up.
+            Join us in this delightful stroll down memory lane, where every click unveils another chapter in the book of your life.
+            Explore, reminisce, and reconnect with the magic of your childhood through the timeless snapshots captured on our platform.
+          </p>
+        </div>
+      </div>
+      
+  );
+}
+
+export default Home;
